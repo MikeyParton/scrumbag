44f34e55daa7823b0c98e29375d269a095d8aca8
Mike Parton <mikeparton@live.com.au>
2018-01-14 19:00:00 -0300

    Add link to bring up card detail modal and finally fix the problem this whole rewrite was meant to fix git add -A :tada:

--- src/features/BoardDetail/AltCard.jsx
+++ src/features/BoardDetail/AltCard.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import { Draggable } from 'react-beautiful-dnd'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+const Container = styled.div`
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`
 
 const CardWrapper = styled.div`
   user-select: none;
@@ -11,23 +19,28 @@ const CardWrapper = styled.div`
 
 class Card extends React.Component {
   render() {
-    const { card, index, theme } = this.props
+    const { card, index } = this.props
+    const { id, name, boardId } = card
+
     return (
       <Draggable
         draggableId={`card-${card.id}`}
         index={index}
       >
         {(provided, snapshot) => (
-          <div>
-            <CardWrapper
-              innerRef={provided.innerRef}
-              {...provided.draggableProps}
-              {...provided.dragHandleProps}
-            >
-              <div>{card.name}</div>
-            </CardWrapper>
-            {provided.placeholder}
-          </div>
+          <Container>
+            <Link to={`/boards/${boardId}/card/${id}`}>
+              <CardWrapper
+                innerRef={provided.innerRef}
+                {...provided.draggableProps}
+                {...provided.dragHandleProps}
+              >
+
+                <div>{card.name}</div>
+              </CardWrapper>
+              {provided.placeholder}
+            </Link>
+          </Container>
         )}
       </Draggable>
     )
