bfff7cbba3ba679244812e84cb701dadf6064945
Mike Parton <mikeparton@live.com.au>
2018-01-15 21:35:59 -0300

    Remove old boards sagas

--- src/app/rootSaga.js
+++ src/app/rootSaga.js
@@ -1,5 +1,5 @@
 import { all, fork } from 'redux-saga/effects'
-import boardsSagas from 'features/Boards/boardsSagas2'
+import boardsSagas from 'features/Boards/boardsSagas'
 import boardDetailSagas from 'features/BoardDetail/boardDetailSagas'
 
 export default function* rootSaga() {
--- src/features/Boards/boardsSagas.js
+++ src/features/Boards/boardsSagas.js
@@ -1,37 +1,13 @@
-import { delay } from 'redux-saga'
-import { takeEvery, put, call } from 'redux-saga/effects'
-import { modalClose } from 'features/ModalManager/modalManagerActions'
-import { resetForm } from 'features/BoardForm/boardFormActions'
-import { BOARDS_REQUEST, CREATE_BOARD_REQUEST } from './boardsConstants'
-import { boardsSuccess, boardsError } from './boardsActions'
-import { createBoardSuccess, createBoardError } from './boardsActions'
-import { boardsRequest, createBoardRequest } from './boardsAPI'
+import { all } from 'redux-saga/effects'
 
-function* boardsRequestSaga() {
-  const { boards, error } = yield call(boardsRequest)
-  if (boards) {
-    yield put(boardsSuccess(boards))
-  } else {
-    yield put(boardsError(error))
-  }
-}
-
-function* createBoardRequestSaga({ payload }) {
-  const { params } = payload
-  const { board, error } = yield call(createBoardRequest, params)
-  if (board) {
-    yield put(createBoardSuccess(board))
-
-    // Small delay to show some Feedback on the form
-    yield delay(500)
-    yield put(resetForm())
-    yield put(modalClose())
-  } else {
-    yield put(createBoardError(error))
-  }
-}
+import {
+  getBoardsRequest,
+  createBoardRequest
+} from './boardsRequests'
 
 export default function* rootSaga() {
-  yield takeEvery(BOARDS_REQUEST, boardsRequestSaga)
-  yield takeEvery(CREATE_BOARD_REQUEST, createBoardRequestSaga)
+  yield all([
+    getBoardsRequest.saga(),
+    createBoardRequest.saga()
+  ])
 }
