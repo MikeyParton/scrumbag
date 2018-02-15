b641234a9c346d1d174435b4534b64e512d913ed
Mike Parton <mikeparton@live.com.au>
2018-02-15 07:10:11 -0300

    Keep url synced after updating card

--- src/features/CardDetail/cardDetailSagas.js
+++ src/features/CardDetail/cardDetailSagas.js
@@ -1,9 +1,16 @@
-import { all } from 'redux-saga/effects'
+import { call, all, takeEvery } from 'redux-saga/effects'
+import history from 'app/Routes/history'
 import { getCardDetailRequest, updateCardRequest } from './cardDetailRequests'
+
+export function* syncUrl({ payload }) {
+  const { slug } = payload.card
+  yield call(history.push, `/c/${slug}`)
+}
 
 export default function* rootSaga() {
   yield all([
     getCardDetailRequest.saga(),
-    updateCardRequest.saga()
+    updateCardRequest.saga(),
+    takeEvery(updateCardRequest.constants.success, syncUrl)
   ])
 }
